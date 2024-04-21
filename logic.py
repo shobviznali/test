6f015052eb90a88f145c49e866655ea86418f2ea
KodlandLVL3 <155145450+KodlandLVL3@users.noreply.github.com>
2024-04-21 19:49:28 +0500

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,8 +1,7 @@
-
 import sqlite3
 from config import DATABASE
 
-skills = [ (_,) for _ in (['Python', 'SQL', 'API'])]
+skills = [ (_,) for _ in (['Python', 'SQL', 'API', 'Telegram'])]
 statuses = [ (_,) for _ in (['На этапе проектирования', 'В процессе разработки', 'Разработан. Готов к использованию.', 'Обновлен', 'Завершен. Не поддерживается'])]
 
 class DB_Manager:
@@ -42,6 +41,7 @@ class DB_Manager:
         conn = sqlite3.connect(self.database)
         with conn:
             conn.executemany(sql, data)
+            conn.commit()
 
     def __select_data(self, sql, data = tuple()):
         conn = sqlite3.connect(self.database)
@@ -52,23 +52,23 @@ class DB_Manager:
             
 
     def default_insert(self):
-        sql = 'INSERT INTO skills (skill_name) values(?)'
+        sql = 'INSERT OR IGNORE INTO skills (skill_name) values(?)'
         data = skills
         self.__executemany(sql, data)
-        sql = 'INSERT INTO status (status_name) values(?)'
+        sql = 'INSERT OR IGNORE INTO status (status_name) values(?)'
         data = statuses
         self.__executemany(sql, data)
 
 
     def insert_project(self, data):
-        sql = 'INSERT INTO projects (user_id, project_name, url, status_id) values(?, ?, ?, ?)'
-        self.__executemany(sql, [data])
+        sql = 'INSERT OR IGNORE INTO projects (user_id, project_name, url, status_id) values(?, ?, ?, ?)'
+        self.__executemany(sql, data)
 
     def insert_skill(self, user_id, project_name, skill):
         sql = 'SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?'
         project_id = self.__select_data(sql, (project_name, user_id))[0][0]
-        skill_id = self.__select_data('SELECT skill_id FROM skill WHERE skill_name = ?', (skill,))[0][0]
-        data = [(skill_id, project_id)]
+        skill_id = self.__select_data('SELECT skill_id FROM skills WHERE skill_name = ?', (skill,))[0][0]
+        data = [(project_id,skill_id)]
         sql = 'INSERT OR IGNORE INTO project_skills VALUES (?, ?)'
         self.__executemany(sql, data)
 
@@ -90,22 +90,20 @@ class DB_Manager:
         return self.__select_data(sql='SELECT project_id FROM projects WHERE project_name = ? AND user_id = ?  ', data = (project_name, user_id,))[0][0]
 
     def get_skills(self):
-        return self.__select_data(sql='SELECT * FROM skill')
+        return self.__select_data(sql='SELECT * FROM skills')
     
     def get_project_skills(self, project_name):
-        res = self.__select_data(sql='''SELECT skill_name FROM 
-(SELECT * FROM projects 
+        res = self.__select_data(sql='''SELECT skill_name FROM projects 
 JOIN project_skills ON projects.project_id = project_skills.project_id 
 JOIN skills ON skills.skill_id = project_skills.skill_id 
-WHERE project_name = ?)''', data = (project_name,) )
+WHERE project_name = ?''', data = (project_name,) )
         return ', '.join([x[0] for x in res])
     
     def get_project_info(self, user_id, project_name):
         sql = """
-SELECT project_name, description, url, status_name FROM  (
-SELECT * FROM projects 
+SELECT project_name, description, url, status_name FROM projects 
 JOIN status ON
-status.status_id = projects.status_id)
+status.status_id = projects.status_id
 WHERE project_name=? AND user_id=?
 """
         return self.__select_data(sql=sql, data = (project_name, user_id))
